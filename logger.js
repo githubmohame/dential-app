a75f7ae02f35c68951d750f7a33f8b9f2d77bac4
Omarjellyfish <omarkandilfan@gmail.com>
2024-10-18 21:57:17 +0300

    logger is currently functional

--- logger.js
+++ logger.js
@@ -1,15 +1,14 @@
 import winston from "winston";
 
 const logger = winston.createLogger({
-  level: "info", // Set the default logging level
+  level: "info",
   format: winston.format.combine(
-    winston.format.timestamp(), // Include timestamps in logs
-    winston.format.json() // Format logs as JSON
+    winston.format.timestamp(),
+    winston.format.json()
   ),
   transports: [
-    new winston.transports.Console(), // Log to console
-    new winston.transports.File({ filename: "error.log", level: "error" }), // Log errors to a file
-    new winston.transports.File({ filename: "combined.log" }), // Log all messages to a file
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
   ],
 });
 
