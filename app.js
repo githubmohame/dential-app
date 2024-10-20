701dd56f4750c736563654c7d69aff1b396c6ca0
mohamed <mohammmedayman5@gmail.com>
2024-10-20 16:59:50 +0300

    test gmail

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-import express from "express";
-import Admin from "./models/admin.model.js";
-import AdminRepos from "./repositories/admin.rep.js";
-import AdminController from "./controllers/admin.controller.js";
-import AdminRouterFun from "./routes/admin_routes/admin.route.js";
-import multer from "multer";
-import UserRepos from "./repositories/user.rep.js";
-import User from "./models/user.model.js";
-import UserController from "./controllers/user.controller.js";
-import UserRouterFun from "./routes/user_routes/user_routes.js";
-import mongoose from "mongoose";
-import logger from "./logger.js";
 import dotenv from "dotenv";
-import ServiceRouterFun from "./routes/service_routes/service_routes.js";
-import serviceModel from "./models/service.model.js";
+import express from "express";
+import mongoose from "mongoose";
+import multer from "multer";
+import AdminController from "./controllers/admin.controller.js";
 import ServiceController from "./controllers/serviceController.js";
+import UserController from "./controllers/user.controller.js";
+import logger from "./logger.js";
+import Admin from "./models/admin.model.js";
+import serviceModel from "./models/service.model.js";
+import User from "./models/user.model.js";
+import AdminRepos from "./repositories/admin.rep.js";
 import ServiceRepo from "./repositories/serviceRepo.js";
+import UserRepos from "./repositories/user.rep.js";
+import AdminRouterFun from "./routes/admin_routes/admin.route.js";
+import ServiceRouterFun from "./routes/service_routes/service_routes.js";
+import UserRouterFun from "./routes/user_routes/user_routes.js";
+import nodemailer from "nodemailer"
 dotenv.config();
 mongoose
-  .connect(process.env.CONNECTION_STRING, {})
-  .then(() => {
+  .connect("mongodb://127.0.0.1:27017/dentalDatabase", {})
+  .then(async() => {
     console.log("Connected to MongoDB");
     logger.info("Connected to MongoDB");
     let userRouter = UserRouterFun(User, UserController, UserRepos);
@@ -43,6 +44,29 @@ mongoose
       //res.send({error:""});
     }
     const port = process.env.port || 5000;
+    const transporter =await nodemailer.createTransport({
+      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "trialmailer206@gmail.com",
+        pass: "pdvfpsaooamwwrdh ",
+      },
+    });
+    const mailOptions = {
+      from: "mohammmedaymansoliman@gmail.com",
+      to: "mohammmedayman5@gmail.com",
+      subject: "Hello from Nodemailer",
+      text: "This is a test email sent using Nodemailer.",
+    };
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email: ", error);
+      } else {
+        console.log("Email sent: ", info.response);
+      }
+    });
     app.use(errorHandler);
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
