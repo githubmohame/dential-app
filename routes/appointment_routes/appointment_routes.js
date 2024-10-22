05d004eae0e707ba114382401b88d85f55411f0c
KarimSharaf2001 <133995508+KarimSharaf2001@users.noreply.github.com>
2024-10-22 20:04:12 +0300

    Update appointment_routes.js

--- routes/appointment_routes/appointment_routes.js
+++ routes/appointment_routes/appointment_routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { getAllAppointments,createAppointment, getAvailableTimes,getUserAppointmentsController,cancelAppointment} from '../../controllers/appointment.controller.js';
+import { getAllAppointmentsDay, getAllAppointmentsMonth, createAppointment, getAvailableTimes,getUserAppointmentsController,cancelAppointment} from '../../controllers/appointment.controller.js';
 
 const router = express.Router();
 
 
 
 router.get('/appointments/available-times/:year/:month/:day', getAvailableTimes);
-router.get('/appointments/:year/:month/:day', getAllAppointments);
+router.get('/appointments/:year/:month/:day', getAllAppointmentsDay);
+router.get('/appointments/:year/:month', getAllAppointmentsMonth);
 router.post('/appointments', createAppointment);
 router.get('/appointments/user/:userId', getUserAppointmentsController);
 router.delete('/appointments/cancel', cancelAppointment);
